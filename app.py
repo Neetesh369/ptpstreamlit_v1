2a2e0e1310f2fa9813273983b350410ed75556bb
tsg2025 <tradingstrategyguru@gmail.com>
2025-03-20 11:44:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
 import io
 import time
+import uuid
 
 # If modifying these SCOPES, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly', 'https://www.googleapis.com/auth/drive.readonly']
